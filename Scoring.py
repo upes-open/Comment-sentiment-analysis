e46de01fdd41cfdc6ab2b5bf160f185c41b56ab8
AnanyaM12 <ananyaamathur03@gmail.com>
2023-04-26 19:44:38 +0530

    adding sentiment scores

---
+++ Scoring.py
@@ -0,0 +1,44 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+# Create an instance of the SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+
+# Iterate through the rows of the DataFrame and apply sentiment analysis on each row
+def scoring(df):
+    new=[]
+    for index, row in df.iterrows():
+        # Extract the text from the "text" column of the current row
+        text = row["Text"]
+        # Apply the sentiment analysis method on the text and append the results to a list
+        scores = sia.polarity_scores(text)
+        new.append(scores)
+        # Create a new DataFrame from the list of sentiment scores
+    sentiment_score=pd.DataFrame(new)
+    return sentiment_score
+
+#for calculating an average count 
+def counting(sentiment_score):
+    countp=0
+    countn=0
+    countneu=0
+
+    for i in sentiment_score.values:
+        if i[0]>i[2]:
+            countn=countn+1
+        elif i[0]<i[2]:
+            countp=countp+1
+        else:
+            countneu=countneu+1
+    return countn,countp,countneu
+    
+
+
+#for making a graphical representation 
+#y=[countp,countn,countneu]
+#mylabels=["positive","negative","neutral"]
+#plt.pie(y,labels=mylabels)
+#plt.show()
